b64afb14bc5d4ae54a334edfe6d9e9c22b4e614c
tunnu005 <tunnuthe005@gmail.com>
2025-02-13 17:50:17 +0530

    notification

---
+++ src/cloudinary.ts
@@ -0,0 +1,18 @@
+import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+cloudinary.config(
+    {
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+        // secure: process.env.NODE_ENV === 'production',
+        // url: process.env.CLOUDINARY_URL,
+        // upload_preset: process.env.CLOUDINARY_PRESET,
+        // proxy: true, // If you're behind a proxy server, uncomment this line.
+    }
+)
+
+export default cloudinary;
